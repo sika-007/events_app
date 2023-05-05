728b7a1348006243bb69d204b066d8236da117b6
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-05 23:23:22 +0100

    Necessary pages created; Both static and dynamic

---
+++ pages/about-us.jsx
@@ -0,0 +1,10 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>This is the about us page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem error, eaque in quae quis alias ducimus ad officiis ab incidunt maxime optio ratione iure rerum explicabo! Nulla in, nisi aperiam quod labore cum quidem vel ab ullam impedit dicta aliquid nam numquam ad beatae eaque autem modi recusandae nostrum minima</p>
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ pages/events/[cat]/[id].jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+/* Pay attention to the way this page is named. It is so to specify that this is a dynamic page that can hold dynamic content */
+
+const EventPage = () => {
+  return (
+    <div>
+      <h1>Our single event</h1>
+    </div>
+  )
+}
+
+export default EventPage
