66a009d13e94ba5d9108b4842c98648cfa078b76
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-06 11:06:40 +0100

    path configuration using getStaticPaths

--- pages/events/[cat]/index.jsx
+++ pages/events/[cat]/index.jsx
@@ -1,14 +1,62 @@
 import React from 'react'
 
-const EventsPerCity = () => {
+export async function getStaticPaths() {
+
+  const { events_categories } = await import("/data/data.json")
+
+  const allPaths = events_categories.map(event => {
+    return {
+      params: {
+        cat: event.id.toString(),
+      }
+    }
+  })
+
+  return {
+    paths: allPaths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps(context) {
+
+  const { all_events } = await import("/data/data.json")
+  const id = context?.params?.cat
+  const data = all_events.filter(event => event.city === id)
+  console.log(data)
+
+  return {
+    props: { data }
+  }
+}
+
+const EventsPerCity = ({ data }) => {
+
+  console.log(data)
+
   return (
     <div>
       <h1>Events in London</h1>
-      <a href="/events/event1">Event 1</a>
-      <a href="/events/event2">Event 2</a>
-      <a href="/events/event3">Event 3</a>
-      <a href="/events/event4">Event 4</a>
-      <a href="/events/event5">Event 5</a>
+      <a href="/events/london/event1">
+        <img src="" alt="" />
+        <h2>Event 1</h2>
+      </a>
+      <a href="/events/london/event2">
+        <img src="" alt="" />
+        <h2>Event 2</h2>
+      </a>
+      <a href="/events/london/event5">
+        <img src="" alt="" />
+        <h2>Event 3</h2>
+      </a>
+      <a href="/events/london/event3">
+        <img src="" alt="" />
+        <h2>Event 4</h2>
+      </a>
+      <a href="/events/london/event4">
+        <img src="" alt="" />
+        <h2>Event 5</h2>
+      </a>
     </div>
   )
 }
