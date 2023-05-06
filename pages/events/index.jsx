0814eb418f48074dad916ed6246019780949acda
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-06 09:51:16 +0100

    data fetching using getStaticProps

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,24 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Events = () => {
+export async function getStaticProps() {
+  const { events_categories } = await import("/data/data.json")
+  return {
+    props: {
+      data: events_categories,
+    }
+  }
+}
 
+const Events = ({ data }) => {
+  console.log(data)
   return (
     <div>
-        <h1>Events Page</h1>
-        <div>
-            <a href="">
-                <img src="" alt="" />
-                <h2>Events in London</h2>
-            </a>
-            <a href="">
-                <img src="" alt="" />
-                <h2>Events in San Francisco</h2>
-            </a>
-            <a href="">
-                <img src="" alt="" />
-                <h2>Events in Barcelona</h2>
-            </a>
-        </div>
+      <h1>Events Page</h1>
+      <div>
+        {data.map((event, index) => (
+          <a key={event.id + index} href={`/events/${event.id}`}>
+            <Image src={event.image} alt={event.title} width={300} height={200} /> <h2>{event.title}</h2>
+          </a>
+        ))}
+      </div>
     </div>
   )
 }
