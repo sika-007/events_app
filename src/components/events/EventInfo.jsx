6d5eed7e8b2760e929e9f2b9e8411da8873c5dec
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-05-11 17:33:17 +0100

    input for collecting emails laid out and styled

--- src/components/events/EventInfo.jsx
+++ src/components/events/EventInfo.jsx
@@ -2,12 +2,22 @@ import Image from 'next/image'
 
 
 const EventInfo = ({ pageItem }) => {
+
+  function handleSubmit() {
+
+  }
+
   return (
     <div className='event_info'>
       <Image className='image' src={pageItem.image} width={1000} height={300} alt={pageItem.title} />
       <h1>{pageItem.title}</h1>
       <p>{pageItem.description}</p>
-      <input type="email" /> <button>Submit</button>
+      <form className='email-registration' onSubmit={handleSubmit}>
+        <label className='input-and-btn'>
+          Get registered for this event
+          <input id="email" type="email" placeholder='Please insert your email' /> <button type="button">Submit</button>
+        </label>
+      </form>
     </div>
   )
 }
